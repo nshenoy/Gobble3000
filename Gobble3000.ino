108efa4747483b62b89d95d09ff55ffbf9b8b29d
Nithin Shenoy <nithin_shenoy@hotmail.com>
2014-11-10 20:30:59 -0500

    Fixes and Cleanup
    - Fixed strip instantiating to take 2 NeoPixels into account
    - Added an 'outer loop' to help adjust breathing rate
    - Formatted, cleaned up, and commented code

--- Gobble3000.ino
+++ Gobble3000.ino
@@ -1,6 +1,14 @@
+/// Gobble3000
+/// Description: A silly first grade school project that manipulates a few LEDs using an Adafruit Gemma and Adafruit NeoPixels.
+///                This is all to light up a picture of a turkey for Thanksgiving. Yes, really. The first NeoPixel will be red 
+///                and fade with a breathing effect, while the second NeoPixel will hold a steady yellow. The onboard LED on the Gemma
+///                is set to blink.
+
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 2
+#define PIN             2    // Pin used for communicating with the NewPixel chain
+#define MAXBRIGHTNESS   255  // Maximum brightness value for the NeoPixel
+const int _onBoardLed = 1;   // Pin used for communicating with the onboard LED
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -9,45 +17,58 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
-int onBoardLed = 1;
+Adafruit_NeoPixel _strip = Adafruit_NeoPixel(2, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() 
 {
-  pinMode(onBoardLed, OUTPUT);
-  
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+    pinMode(_onBoardLed, OUTPUT);
+
+    // Setup the NeoPixels
+    _strip.begin();
+    _strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() 
 {
-  int neoPixelBrightness = 0;
-  int onBoardLedValue = LOW;
-  
-  for(int i = 0; i < 510; i++)
-  {
-    // Set NeoPixel brightness
-    if (i > 255)
-    {
-      neoPixelBrightness = 255 - (i - 255);
-      onBoardLedValue = LOW;
-    }
-    else
-    {
-      neoPixelBrightness = i;
-      onBoardLedValue = HIGH;
-    }
-    
-    strip.setPixelColor(0, strip.Color(neoPixelBrightness, 0, 0));
-    
-    // Set NeoPixel to Yellow
-    strip.setPixelColor(1, strip.Color(255, 255, 0));
-    
-    strip.show();
-    digitalWrite(onBoardLed, onBoardLedValue);
-    
-    delay(2);
-  }
-}
+    const int MAXLOOPCOUNT = 1;    // Adjust this value to set the breathing effect to your taste
+    int neoPixelBrightness = 0;
+    int onBoardLedValue = LOW;
 
+    // Set the second NeoPixel to Yellow
+    _strip.setPixelColor(1, _strip.Color(255, 150, 0));
+
+    // This outer loop gives a better "breathing" effect when fading the first NeoPixel.
+    // By skipping an iteration or two (configuraable via the MAXLOOPCOUNT value), it gives a nice
+    //  pause between the "exhale" and the subsequent "inhale".
+    for(int outerloop = 0; outerloop < MAXLOOPCOUNT; outerloop++)
+    {
+        // Set NeoPixel brightness for the breathing fade effect
+        for(int i = 0; i < (MAXBRIGHTNESS * 2); i++)
+        {
+            if (i > MAXBRIGHTNESS)
+            {
+                // Inhale
+                neoPixelBrightness = MAXBRIGHTNESS - (i - MAXBRIGHTNESS);
+                onBoardLedValue = LOW;
+            }
+            else
+            {
+                // Exhale
+                neoPixelBrightness = i;
+                onBoardLedValue = HIGH;
+            }
+
+            // Only change the NeoPixel if we're on the first iteration of the outerloop
+            if(outerloop == 0)
+            {
+                _strip.setPixelColor(0, _strip.Color(neoPixelBrightness, 0, 0));
+            }
+
+            _strip.show();
+
+            digitalWrite(_onBoardLed, onBoardLedValue);
+
+            delay(5);
+        }
+    }
+}
